88767b7e58aab4630257c5e7e3357233d45cba0b
Angel Pinto <pintodiazangel@gmail.com>
2026-02-22 22:46:55 +0100

    Add project files.

---
+++ ProyectoGestorPeluqueria/Controllers/GestionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoGestorPeluqueria.wwwroot
+{
+    public class GestionController : Controller
+    {
+        public IActionResult RegistrarNegocio()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoGestorPeluqueria/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoGestorPeluqueria.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+    }
+}
